c41409ec1109bc031494f4c616fa8e39acc2764a
Alunos <Alunos>
2017-03-06 20:03:27 -0300

    *

--- server2.php
+++ server2.php
@@ -4,7 +4,7 @@
 $servidor = "localhost";
 $usuario_bd = "root";
 $banco = "maumau";
-$con = mysql_connect ($servidor, $usuario_bd);
+$con = mysql_connect($servidor, $usuario_bd);
 mysql_select_db($banco);
 
 require_once('nusoap/lib/nusoap.php');
@@ -21,14 +21,15 @@ $server->register('alunosMatriculados',
 	'rpc',
 	'encoded',
 	'Retorna os alunos');
-$query = mysql_query("SELECT * FROM disciplinas WHERE name = '".$name."'");
-$id_disciplina = mysql_result($query,0,'id');
-$query2 = mysql_query("SELECT * FROM alunos,turmas WHERE turmas.id_disciplina=".$id_disciplina." AND turmas.id_aluno=alunos.id GROUP BY alunos.nome");
- $resultado = mysql_fetch_array($query);
+
 
 function alunosMatriculados($name)
 {
-    return $resultado;
+	$query = mysql_query("SELECT * FROM disciplina WHERE nome = '".$name."'");
+	$id_disciplina = mysql_result($query,0,'id');
+	$query2 = mysql_query("SELECT * FROM aluno,turmas WHERE turmas.id_disciplina=".$id_disciplina." AND turmas.id_aluno=aluno.id GROUP BY aluno.nome");
+	$resultado = mysql_fetch_array($query);
+    return $resultado[0];
 }
 
 $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
--- x.php
+++ x.php
@@ -1,13 +1,12 @@
-<?php 
-
+<?php
 $con = mysql_connect ('localhost', 'root');
 mysql_select_db('maumau');
-$query = mysql_query("SELECT * FROM alunos,turmas WHERE turmas.id_disciplina=1 AND turmas.id_aluno=alunos.id GROUP BY alunos.nome");
+$id_disciplina = 1;
+$query = mysql_query("SELECT * FROM aluno,turmas WHERE turmas.id_disciplina=".$id_disciplina." AND turmas.id_aluno=aluno.id GROUP BY aluno.nome");
 $resultado = mysql_fetch_array($query);
 while($linha = mysql_fetch_array($query)){
 	echo $linha["nome"];
 }
-		 
 mysql_close($con);
 
  ?>
